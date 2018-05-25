8d81d293049395085c5b59f593eb67d2cf41a546
ethanryan <ethanryan@gmail.com>
2018-05-25 12:31:23 -0400

    chat app and edit app now both in index.html

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ io.on('connection', function(socket){
   });
 });
 
+//beow if for chat messaging
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     console.log('message: ' + msg);
@@ -22,6 +23,19 @@ io.on('connection', function(socket){
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
+  });
+});
+
+//below is for textarea update in myFormEdit
+io.on('connection', function(socket){
+  socket.on('textarea update', function(msg){
+    console.log('textarea update: ' + msg);
+  });
+});
+
+io.on('connection', function(socket){
+  socket.on('textarea update', function(msg){
+    io.emit('textarea update', msg);
   });
 });
 
